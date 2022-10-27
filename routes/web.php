f3d34c30a346a518e319d1e62f81c4d58e8cb443
Tommy-S05 <tommy-s05f@hotmail.com>
2022-10-27 10:22:52 -0400

    Video de Maquetacion terminado

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/post', [App\Http\Controllers\HomeController::class, 'post'])->name('post');
 
 Auth::routes();
 
